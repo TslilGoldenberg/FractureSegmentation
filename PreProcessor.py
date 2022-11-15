5c3289a563fe012e33b11ebee6411f3fe24f2f87
Tzlil Ovadia <tzlil.ovadia@mail.huji.ac.il>
2022-11-15 14:03:09 +0200

    adding preprocessor

---
+++ PreProcessor.py
@@ -0,0 +1,17 @@
+
+
+class PreProcessor:
+    def __init__(self):
+        pass
+
+    def applyThreshold(self, minTH, maxTH):
+        """
+
+        :param minTH:
+        :param maxTH:
+        :return:
+        """
+        pass
+
+
+
