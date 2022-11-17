4835e0a8721bc1c3e2e1a13edec5c755c0f3d89a
Tzlil Ovadia <tzlil.ovadia@mail.huji.ac.il>
2022-11-17 14:27:55 +0200

    thresholding stage is finished

--- PreProcessor.py
+++ PreProcessor.py
@@ -40,16 +40,14 @@ class PreProcessor:
         :param Imax:
         :return:
         """
-        MAX_VOXEL_VALUE = 65535
-        MIN_VOXEL_VALUE = 0
         img = nib.load(nifty_file)
         img_data = img.get_fdata().astype(dtype=np.uint16)
         img_data[(img_data <= Imax) & (img_data > Imin)] = MAX_VOXEL_VALUE
         img_data[img_data < MAX_VOXEL_VALUE] = MIN_VOXEL_VALUE
-        opened_img = skimage.morphology.opening(img_data)
+        opened_img = skimage.morphology.closing(img_data)
         final_image = nib.Nifti1Image(opened_img, img.affine)
         nib.save(final_image, f"out_seg_{Imin}_{Imax}.nii.gz.")
-        self.img = opened_img
+        self.img_data = opened_img
         return opened_img
 
     def SkeletonTHFinder(self, nifty_file):
@@ -64,62 +62,17 @@ class PreProcessor:
         return the Imin used for that.
         :return:
         """
-        Imin_range = np.arange(136, 178, 14)
+        Imin_range = np.arange(150,250, 14)
         best_result = np.inf
         img_res = None
         for i_min in Imin_range:
             self.SegmentationByTH(nifty_file, i_min, 1300)
-            labels, cmp = label(self.img, return_num=True)
+            labels, cmp = label(self.img_data, return_num=True)
             print(f"cmp is {cmp}")
             if best_result > cmp:
                 best_result = cmp
                 img_res = self.img_data
         return img_res
-
-
-
-    # def SegmentationByTH(self, Imin, Imax):
-    #     """
-    #     This function is given as inputs a grayscale NIFTI file (.nii.gz) and two integers – the minimal and maximal thresholds. The function generates a segmentation NIFTI file of the same dimensions, with a binary segmentation – 1 for voxels between Imin and Imax, 0 otherwise. This segmentation NIFTI file should be saved under the name <nifty_file>_seg_<Imin>_<Imax>.nii.gz.
-    #     The function returns 1 if successful, 0 otherwise. Preferably, raise descriptive errors when returning 0.
-    #     :param nifty_file:
-    #     :param Imin:
-    #     :param Imax:
-    #     :return:
-    #     """
-    #     self.img_data[(self.img_data <= Imax) & (self.img_data > Imin)] = MAX_VOXEL_VALUE
-    #     self.img_data[self.img_data < MAX_VOXEL_VALUE] = MIN_VOXEL_VALUE
-    #     opened_img = opening(self.img_data)
-    #     # final_image = nib.Nifti1Image(opened_img, self.img.affine)
-    #     # nib.save(final_image, f"out_seg_{Imin}_{Imax}.nii.gz.")
-    #     self.img_data = opened_img
-
-    # def SkeletonTHFinder(self):
-    #     """
-    #     This function iterates over 25 candidate Imin thresholds in the range of [150,500] (with intervals of 14).
-    #     In each run, use the SegmentationByTH function you’ve implemented, and count the number of connectivity components
-    #     in the resulting segmentation with the current Imin. Plot your results – number of connectivity components per Imin. Choose the Imin which is the first or second minima in the plot. Also, make sure to include that graph in your report.
-    #
-    #     Next, you need to perform post-processing (morphological operations – clean out single pixels, close holes, etc.)
-    #     until you are left with a single connectivity component.
-    #     Finally, this function should save a segmentation NIFTI file called “<nifty_file>_SkeletonSegmentation.nii.gz” and
-    #     return the Imin used for that.
-    #     :return:
-    #     """
-    #     Imin_range = np.arange(90, 164, 14)
-    #     self.SegmentationByTH(220, 1300)
-    #     best_result = np.inf
-    #     img_res = None
-    #     for i_min in Imin_range:
-    #         img = self.img_data.copy()
-    #         self.SegmentationByTH(i_min, 1300)
-    #         labels, cmp = label(img, return_num=True)
-    #         print(f"cmp is {cmp}")
-    #         if best_result > cmp:
-    #             best_result = cmp
-    #             img_res = img
-    #             print(i_min)
-    #     return img_res
 
     def _calculatePelvicLayersBoundaries(self):
         pass
@@ -149,10 +102,12 @@ class PreProcessor:
     def get_Nlargest_component(self):
         labels = label(self.bones)
         largestCC = labels == np.argmax(np.bincount(labels.flat)[1:]) + 1
-        final_image = nib.Nifti1Image(self.img_data*largestCC, self.raw_img.affine)
+        largestCC_img = self.bones*largestCC
+        largestCC_img = skimage.morphology.closing(largestCC_img)
+        final_image = nib.Nifti1Image(largestCC_img, self.raw_img.affine)
         nib.save(final_image, f"out_largestCC.nii.gz")
 
 if __name__ == "__main__":
-    ob = PreProcessor(nifty_file='datasets/case2.nii.gz')
+    ob = PreProcessor(nifty_file='datasets/case1.nii.gz')
     ob._extractPelvic()
     ob.get_Nlargest_component()
