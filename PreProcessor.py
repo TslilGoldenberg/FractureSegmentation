46676cf0182256a0d9034793f7c306fb94a68427
Tzlil Ovadia <tzlil.ovadia@mail.huji.ac.il>
2022-11-24 19:59:40 +0200

    removed redundant code causing a significant slow-down in performance

--- PreProcessor.py
+++ PreProcessor.py
@@ -5,9 +5,15 @@ from skimage.morphology import (erosion, dilation, closing, opening,
                                 area_closing, area_opening)
 import nibabel as nib
 import cv2 as cv2
-import dicom2nifti
+import dcm2niix
 import os
 from matplotlib import pyplot as plt
+from skimage.exposure import equalize_adapthist, equalize_hist
+import torch
+import torch.optim as optim
+import torch.nn as nn
+from torchviz import make_dot
+import dicom2nifti
 
 MAX_VOXEL_VALUE = 65535
 MIN_VOXEL_VALUE = 0
@@ -22,6 +28,7 @@ class PreProcessor:
         self.img_data = self.raw_img.get_fdata().astype(dtype=np.uint16)
         _, _, z_slices = np.nonzero(self.img_data)
         self.bottom_bound_slice, self.top_bound_slice = np.min(z_slices), np.max(z_slices)
+        self.equalized_img = None
         self.bones = None
 
     def _applyThreshold(self, minTH, maxTH):
@@ -33,7 +40,7 @@ class PreProcessor:
         """
         pass
 
-    def SegmentationByTH(self, nifty_file, Imin, Imax):
+    def SegmentationByTH(self, Imin, Imax):
         """
         This function is given as inputs a grayscale NIFTI file (.nii.gz) and two integers – the minimal and maximal thresholds. The function generates a segmentation NIFTI file of the same dimensions, with a binary segmentation – 1 for voxels between Imin and Imax, 0 otherwise. This segmentation NIFTI file should be saved under the name <nifty_file>_seg_<Imin>_<Imax>.nii.gz.
         The function returns 1 if successful, 0 otherwise. Preferably, raise descriptive errors when returning 0.
@@ -42,17 +49,19 @@ class PreProcessor:
         :param Imax:
         :return:
         """
-        img = nib.load(nifty_file)
-        img_data = img.get_fdata().astype(dtype=np.uint16)
-        img_data[(img_data <= Imax) & (img_data > Imin)] = MAX_VOXEL_VALUE
-        img_data[img_data < MAX_VOXEL_VALUE] = MIN_VOXEL_VALUE
-        opened_img = skimage.morphology.closing(img_data)
-        final_image = nib.Nifti1Image(opened_img, img.affine)
-        nib.save(final_image, f"out_seg_{Imin}_{Imax}.nii.gz.")
-        self.img_data = opened_img
+        if self.equalized_img is None:
+            self.equalized_img = np.copy(self.img_data)
+            self.equalized_img = equalize_adapthist(self.equalized_img, nbins=MAX_VOXEL_VALUE) * (MAX_VOXEL_VALUE//4)
+            self.equalized_img = self.equalized_img.astype(dtype=np.uint16)
+
+
+        self.equalized_img[(self.equalized_img <= Imax) & (self.equalized_img > Imin)] = MAX_VOXEL_VALUE
+        self.equalized_img[self.equalized_img < MAX_VOXEL_VALUE] = MIN_VOXEL_VALUE
+        opened_img = skimage.morphology.closing(self.equalized_img)
         return opened_img
 
-    def SkeletonTHFinder(self, nifty_file):
+
+    def SkeletonTHFinder(self):
         """
         This function iterates over 25 candidate Imin thresholds in the range of [150,500] (with intervals of 14).
         In each run, use the SegmentationByTH function you’ve implemented, and count the number of connectivity components
@@ -64,39 +73,36 @@ class PreProcessor:
         return the Imin used for that.
         :return:
         """
-        Imin_range = np.arange(150,250, 14)
+        Imin_range = np.arange(150, 300, 10)
         best_result = np.inf
         img_res = None
         for i_min in Imin_range:
-            self.SegmentationByTH(nifty_file, i_min, 1300)
+            img = self.SegmentationByTH(i_min, 1300)
             labels, cmp = label(self.img_data, return_num=True)
-            print(f"cmp is {cmp}")
+            if cmp == 0:
+                break
             if best_result > cmp:
                 best_result = cmp
-                img_res = self.img_data
+                img_res = img
         return img_res
 
-    def _calculatePelvicLayersBoundaries(self):
-        pass
 
-    def _slice_process(self):
-        out = np.zeros(self.img_data.shape)
-        for slice in range(self.bottom_bound_slice, self.top_bound_slice):
-            frame = self.img_data[:, :, slice]
-            frame = cv2.equalizeHist(frame.astype(np.uint8))
-            out[:, :, slice] = frame
-        return out
+    def _extractPelvic(self):
+        # Step 1: Apply Adaptive Histogram Equalizer
 
-
-
+        # Step 2: Apply Thresholding for Skeleton
+        self.bones = self.SkeletonTHFinder()
+        final_image = nib.Nifti1Image(self.bones, self.raw_img.affine)
+        nib.save(final_image, "out_seg.nii.gz")
         # Step 3: Get a general ROI
-    def get_Nlargest_component(self):
+
+    def get_Nlargest_component(self, output_directory=""):
         labels = label(self.bones)
         largestCC = labels == np.argmax(np.bincount(labels.flat)[1:]) + 1
         largestCC_img = self.bones*largestCC
         largestCC_img = skimage.morphology.closing(largestCC_img)
         final_image = nib.Nifti1Image(largestCC_img, self.raw_img.affine)
-        nib.save(final_image, "out_largestCC.nii.gz")
+        nib.save(final_image, f"{output_directory}out_largestCC.nii.gz")
 
     def get_pelvis_ROI(self):
         bin_res = np.zeros(self.img_data.copy().shape)
@@ -118,7 +124,11 @@ class PreProcessor:
 
 
 if __name__ == "__main__":
-    ob = PreProcessor(nifti_file='out_largestCC.nii.gz')
-    # ob.get_pelvis_ROI()
-    # ob.get_Nlargest_component()
+    for i in range(1, 2):
+        print(f"working on {i}")
+        output_directory=f"output_directory_{i}/"
+        nifty_file = f'{output_directory}0_anonymous.nii.gz'
+        ob = PreProcessor(nifti_file=nifty_file)
+        ob._extractPelvic()
+        ob.get_Nlargest_component(output_directory=output_directory)
 
